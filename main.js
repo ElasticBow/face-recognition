c556ed64df720f23669d9690aef43d5461eaadea
ElasticBow <79611589+ElasticBow@users.noreply.github.com>
2021-08-24 21:21:49 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function take_snapshot()
 
 console.log('ml5 version', ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/r8RRokvAb/',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/8G4VAp2bQ/',modelLoaded);
 
 function modelLoaded()
 {
